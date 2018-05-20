04bdc4a2232b03f982ecc46fdb65f14004b06085
Dina Hafez <dinahafez.8@gmail.com>
2018-05-20 18:27:49 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function getFirstSelector(selector) {
+  const firstSelector = document.querySelector(selector);
+  return firstSelector;
+}
+
+function nestedTarget() {
+  const target = document.querySelector("#nested .target");
+  return target;
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll(".ranked-list li");
+  for(let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML)+n;
+  }
+  return lis;
+}
+
+function deepestChild() {
+  let node = document.querySelector("#grand-node");
+  let nextNode = node.children[0];
+  
+  while(nextNode) {
+    node = nextNode;
+    nextNode = node.children[0];
+  }
+  return node;
+}
